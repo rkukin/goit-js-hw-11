8b1437499e39c0cc7d3073e33958d994ae5b7d02
rkukin <romakukin@gmail.com>
2019-10-16 10:26:29 +0300

    task 1, 2

--- js/task1.js
+++ js/task1.js
@@ -1,2 +1,35 @@
 "use strict";
 
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const startButton = document.querySelector('[data-action="start"]');
+
+const stopButton = document.querySelector('[data-action="start"]');
+
+const body = document.querySelector('body');
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const picColor = () => {
+  body.style.backgroundColor = colors[randomIntegerFromInterval(0,colors.length)]
+  startButton.setAttribute('disabled', true)
+}
+
+const stopPicker = () => {
+  stopButton.removeAttribute('disabled')
+
+}
+
+startButton.addEventListener('click', picColor)
+
+stopButton.addEventListener('click', stopPicker)
+
--- js/task2.js
+++ js/task2.js
@@ -1,1 +1,26 @@
 "use strict";
+
+
+const delay = ms => {
+  // Твой код
+// return new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve(ms);
+//   }, ms);
+// }) 
+
+const callback = function(resolve, reject){
+  setTimeout(()=>{resolve(ms)},ms)
+}
+
+const promise = new Promise (callback);
+return promise;
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+// Вызовы функции для проверки
+console.log(delay(20000));
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
